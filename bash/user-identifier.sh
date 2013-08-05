a7909da7a5b945adf987bbf4f6c400cbe2e67e56
lnx-7pixel1 <nautilus@shoppydoo.com>
2013-08-05 12:48:15 +0200

    Aggiunte variabili per i messaggi da mostrare.

--- bash/user-identifier.sh
+++ bash/user-identifier.sh
@@ -2,11 +2,15 @@
 # Lo user identifier 0 è particolare in quanto identifica il superuser, o root.
 
 ROOT_UID=0
+ROOT_MESSAGE="Ciao utente ROOT!"
+USER_MESSAGE="Non sei ROOT!"
 
 if [ $UID -eq $ROOT_UID ]
 then
-  echo "Ciao utente ROOT!"
+  echo $ROOT_MESSAGE
 else
-  echo "Non sei ROOT!"
+  echo $USER_MESSAGE 
 fi
+
+
 
