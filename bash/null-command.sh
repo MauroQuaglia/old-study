600cbdd45d65eb3bd2d833a21544e3f833a0d2f7
lnx-7pixel4 <nautilus@shoppydoo.com>
2013-08-26 12:54:40 +0200

    I :

---
+++ bash/null-command.sh
@@ -0,0 +1,14 @@
+# : è il comando null. Sinonimo di true, il suo exit status è 0.
+:
+echo $? # 0
+
+# Fornisce un segnaposto dove è attesa un'operazione binaria.
+: ${nomeutente=`whoami`}
+
+# Valuta una stringa di variabili utilizzando la sostituzione di parametro.
+# Visualizza un messaggio d'errore se una, o più, delle variabili fondamentali d'ambiente non è impostata.
+: ${HOSTSNAME}
+
+# In combinazione con >, l'operatore di redirezione, azzera il contenuto di un file, senza cambiarne i permessi. Se il file non esiste, viene creato.
+: > data.txt
+
