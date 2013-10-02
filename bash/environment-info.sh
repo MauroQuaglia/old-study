ee5e86ae94017982ba083c923c6cc411748b0d76
lnx-7pixel1 <nautilus@shoppydoo.com>
2013-10-02 12:56:03 +0200

    info.

--- bash/environment-info.sh
+++ bash/environment-info.sh
@@ -1,7 +1,25 @@
+RED='\e[0;31m'
+NO_COLOR='\e[0m'
+
 print()
 {
 COMMAND="${!1}"
-echo "$1: [$COMMAND] - $2"
+
+if [[ -z "$COMMAND" ]]
+then 
+ echo -e "$1: [$COMMAND] - ${RED}$2${NO_COLOR}"
+else
+ echo "$1: [$COMMAND] - $2"
+fi
+}
+
+print_groups()
+{
+echo "Array dei numeri id dei gruppi a cui appartiene l'utente corrente." 
+for id in "${GROUPS[@]}"
+do
+   echo "$id"  
+done
 }
 
 print "BASH" "Percorso dell'eseguibile Bash."
@@ -10,11 +28,21 @@ print "BASH_SUBSHELL" "Livello di annidamento delle subshell."
 print "BASH_VERSINFO[0]" "Numero della major version."
 print "BASH_VERSINFO[1]" "Numero della minor version."
 print "BASH_VERSINFO[2]" "Numero del patch level"
+print "BASH_VERSINFO[3]" "Numero della versione di build."
+print "BASH_VERSINFO[4]" "Stato della release."
+print "BASH_VERSINFO[5]" "Architettura."
+print "BASH_VERSION" "La versione Bash installata."
+print "DIRSTACK" "La locazione più alta dello stack delle directory."
+print "EDITOR" "Editor di testo predefinito."
+print "EUID" "Identificativo associato all'identità assunta dall'utente."
+print "FUNCNAME" "Nome della funzione in corrente."
+print "GLOBIGNORE" "Elenco di nomi di file da escludere dalla ricerca nel globbing."
+print_groups  
 
-# BASH_VERSINFO[3] = 1                      # Nr. della build version.
-# BASH_VERSINFO[4] = release                # Stato della release.
-# BASH_VERSINFO[5] = i386-redhat-linux-gnu  # Architettura.
-                                            # (uguale a $MACHTYPE).
+printf -- '%s\n' "${GROUPS[@]}"
+    
 
 
+                                            
+                                            
 
