1ddf8293a891d525c026b23f97e79cab2dae04fd
lnx-7pixel4 <nautilus@shoppydoo.com>
2013-10-21 14:55:53 +0200

    sostituzione di parameri

--- bash/sostituzione-di-parametro/manipolare-ed-espandere.sh
+++ bash/sostituzione-di-parametro/manipolare-ed-espandere.sh
@@ -1,10 +1,13 @@
 # $parametro oppure ${parametro}.
-# In alcuni contesti funziona solo la seconda versione.
 
 PARAM="X"
 echo "$PARAM"
-echo "${PARAM}"
+echo "${PARAM}" # Questa è meno ambigua. In alcuni contesti funziona solo questa.
 
 # Può essere utilizzato per concatenare delle stringhe alle variabili.
 your_id=${USER}-on-${HOSTNAME}
 echo "$your_id"
+
+echo "Vecchio = ${PATH}"
+PATH=${PATH}:/opt/bin  # Aggiunge /opt/bin a $PATH per la durata dello script.
+echo "Nuovo   = ${PATH}"
