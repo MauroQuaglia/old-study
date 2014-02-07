72c3dd5eb9e1a1518256a2d5af8e052007692795
teamnautilus <nautilus@shoppydoo.com>
2014-02-07 12:19:23 +0100

    creato sito vuoto.

---
+++ site/esperimento/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Esperimento::Application.config.secret_token = '3d0f92de0e94d0ca469094eda21f40e9df532f4ac1aa30512e06aaeb9e8614b75df9e02b1b386e4db1f1899f200101308c78f21f48496266973a04f35cc52958'
