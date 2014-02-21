fa48385d9635871fb3915bed96c8cec0c6911cf8
teamnautilus <nautilus@shoppydoo.com>
2014-02-21 12:55:27 +0100

    prima implementazione.

--- kata/RomanNumbers/roman.rb
+++ kata/RomanNumbers/roman.rb
@@ -1,7 +1,26 @@
 class Roman
 
-  def to_decimal(roman)
-    1
+  def initialize
+    @roman = {
+        'I' => 1,
+        'V' => 5
+    }
   end
+
+  def to_decimal(roman)
+    return @roman[roman] if @roman.has_key?(roman)
+
+    sum = 0
+    for i in 0...roman.length - 1
+      sum += sign(roman[i], roman[i+1]) * @roman[roman[i]]
+    end
+    sum + @roman[roman.split(//).last]
+  end
+
+
+  def sign(first, second)
+    @roman[first] < @roman[second] ? -1 : 1
+  end
+
 
 end
--- kata/RomanNumbers/test/roman_test.rb
+++ kata/RomanNumbers/test/roman_test.rb
@@ -7,4 +7,20 @@ class RomanTest < Test::Unit::TestCase
     assert_equal 1, Roman.new.to_decimal('I')
   end
 
+  def test_two
+    assert_equal 2, Roman.new.to_decimal('II')
+  end
+
+  def test_three
+    assert_equal 3, Roman.new.to_decimal('III')
+  end
+
+  def test_four
+    assert_equal 4, Roman.new.to_decimal('IV')
+  end
+
+  def test_prova
+   # puts 1.
+  end
+
 end
