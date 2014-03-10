c9db055eb72f3d453db2b183ead99b9372953f21
teamnautilus <nautilus@shoppydoo.com>
2014-03-10 12:57:42 +0100

    Shots

--- kata/Bowling/shots.rb
+++ kata/Bowling/shots.rb
@@ -1,13 +1,18 @@
 class Shots
 
   def initialize(shots)
-    @shots = []
-    shots.each_with_index { |v, i| i%2==0 && v==10 ? @shots.push(v).push(nil) : @shots.push(v) }
+    @shots = shots.each_with_index { |v, i| shots.insert(i.succ, nil) if strike?(i, v) }
   end
 
   def at_frames(number)
     i=number-1
-    @shots[i*2..(i*2)+1]
+    @shots[i*2..(i*2)+1].compact
+  end
+
+  private
+
+  def strike?(i, v)
+    i%2==0 && v==10
   end
 
 end
--- kata/Bowling/test/shots_test.rb
+++ kata/Bowling/test/shots_test.rb
@@ -21,5 +21,20 @@ class ShotsTest < Test::Unit::TestCase
     assert_equal [3, 4], shots.at_frames(2)
   end
 
+  def test_third_frame_shots_when_strike_in_first_and_second_frame
+    shots = Shots.new [10, 10, 1, 2]
+
+    assert_equal [1, 2], shots.at_frames(3)
+  end
+
+  def test_multiple
+    shots = Shots.new [10, 1, 9, 10, 2, 3]
+
+    assert_equal [10], shots.at_frames(1)
+    assert_equal [1, 9], shots.at_frames(2)
+    assert_equal [10], shots.at_frames(3)
+    assert_equal [2, 3], shots.at_frames(4)
+  end
+
 
 end
