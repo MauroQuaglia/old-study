be6288e3891f000056c561e208552271fbf27320
teamnautilus <nautilus@shoppydoo.com>
2015-01-15 17:22:17 +0100

    Mixin.

--- kata/RubyRookie/Sharing Functionality: Inheritance, Modules, and Mixins/mixin_test.rb
+++ kata/RubyRookie/Sharing Functionality: Inheritance, Modules, and Mixins/mixin_test.rb
@@ -1,4 +1,5 @@
 require 'test/unit'
+require_relative 'person'
 require_relative 'phonograph'
 require_relative 'eight_track'
 
@@ -7,6 +8,35 @@ class MixinTest < Test::Unit::TestCase
   def test_1
     p Phonograph.new('XXX').who_am_i?
     p EightTrack.new('YYY').who_am_i?
+  end
+
+  def test_2
+    # il modulo Comparable presuppone che nella classe sia definto il metodo <=>
+    # fatto questo ho sei funzioni di comparazione gratis! <, <=, ==, >=, >, <=>
+
+    p 'a' < 'b'
+    p 'a' <= 'b'
+    p 'a' == 'b'
+    p 'a' >= 'b'
+    p 'a' > 'b'
+
+    p 'a' <=> 'b'
+    p 'a' <=> 'a'
+    p 'c' <=> 'a'
+  end
+
+  def test_3
+    p1 = Person.new('matz')
+    p2 = Person.new('guido')
+    p3 = Person.new('larry')
+
+    # dato che person include il compare ho gratis i sei metodi di comparazione
+    puts p1 < p2
+
+    #dato che il sort su un array usa l'operatore <=> posso ordinare un array di persone
+    puts [p3, p1, p2].sort
+
+
   end
 
 end
--- kata/RubyRookie/Sharing Functionality: Inheritance, Modules, and Mixins/person.rb
+++ kata/RubyRookie/Sharing Functionality: Inheritance, Modules, and Mixins/person.rb
@@ -1,4 +1,6 @@
 class Person
+  attr_reader :name
+  include Comparable
 
   def initialize(name)
     @name = name
@@ -6,6 +8,12 @@ class Person
 
   def to_s
     "Person named #{@name}"
+  end
+
+  def <=>(other)
+    # il < di person chiama questo metodo per decidere che comparazione usare.
+    # in questo caso è un compare sulle stringhe in ordine alfabetico
+    self.name <=> other.name
   end
 
 end
