c0a6f59411b1a22ee3dff31bf9fcadb09fdc7928
teamnautilus <nautilus@shoppydoo.com>
2015-02-20 12:53:56 +0100

    eccezioni.

--- kata/RubyRookie/Exceptions, catch, and throw/exceptions_test.rb
+++ kata/RubyRookie/Exceptions, catch, and throw/exceptions_test.rb
@@ -3,9 +3,25 @@ require 'test/unit'
 class ExceptionsTest < Test::Unit::TestCase
 
   def test_1
-
+   File.open('xxx.txt')
   end
 
+  def test_2
+    begin
+      raise ArgumentError
+    rescue ArgumentError => boom
+      puts "XXXXXXXXXXXX#{boom.backtrace}"
+    ensure
+      puts 'ensure'
+    end
+  end
+
+  def test_3
+    #raise -> RuntimeError
+    #raise 'mp3' -> RuntimeError con messaggio
+    #raise NameError, 'mp3'  -> NameError con messaggio mp3
+    raise NameError, 'mp3'
+  end
 
 end
 
