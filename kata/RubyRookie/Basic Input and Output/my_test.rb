8a0e4942bfadf8d190e9481e2765132d4bca1daf
teamnautilus <nautilus@shoppydoo.com>
2015-02-26 12:53:46 +0100

    Debug.

--- kata/RubyRookie/Basic Input and Output/my_test.rb
+++ kata/RubyRookie/Basic Input and Output/my_test.rb
@@ -1,10 +1,17 @@
 require 'test/unit'
+require_relative 'book_in_stock'
 
 class MyTest < Test::Unit::TestCase
 
   def test_1
     file = File.new('test-file', 'w')
     file.close
+  end
+
+  def test_2
+    b = Precedence.new()
+    puts b.one
+    puts b.two
   end
 
 end
