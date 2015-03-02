ef9e3707ab82b2f314bdde32128a9e3a13c50c25
teamnautilus <nautilus@shoppydoo.com>
2015-03-02 12:52:01 +0100

    Capitolo 1.

---
+++ kata/RubyRookie/getting-started/my-program-2.rb
@@ -0,0 +1,5 @@
+puts 'Hello'
+puts "It is now: #{Time.now}"
+
+# Posso eseguirlo con ruby my-program-2.rb
+# In questo caso non serve la notazione shebang.
---
+++ kata/RubyRookie/getting-started/my-program.rb
@@ -0,0 +1,6 @@
+#!/usr/bin/env ruby
+puts 'Hello'
+puts "It is now: #{Time.now}"
+
+# Se lo voglio eseguire con ./my-program.rb devo usare la notazione shebang
+# In ogni caso posso sempre eseguirlo come ruby my-program.rb
