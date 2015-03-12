58b8a2365ec7640f68653afaa26a5d266df3c8f4
teamnautilus <nautilus@shoppydoo.com>
2015-03-12 11:23:59 +0100

    Renaming.

---
+++ kata/RubyRookie/classes-objects-and-variables/book_in_stock_test.rb
@@ -0,0 +1,58 @@
+require 'test-unit'
+require_relative 'book_in_stock'
+
+class BookInStockTest < Test::Unit::TestCase
+
+  def test_1
+    b1 = BookInStock.new 'isbn1', 3
+    p b1
+    b2 = BookInStock.new 'isbn2', 3.14
+    p b2
+    b3 = BookInStock.new 'isbn3', '5.67'
+    p b3
+    puts b3 #il puts chiama il metodo to_s
+  end
+
+  def test_2
+    b = BookInStock.new 'isbn', 33.80
+    p b.isbn
+
+    p b.price
+    p b.price_in_cents
+    b.price = b.price * 0.75 # ho usato il setter. Prima dell'aggiustamento avevo 25.349999999999998
+    p b.price
+    p b.price_in_cents
+  end
+
+  def test_3
+    a = BookInStock.new '', 10
+    p a.type_of_price
+
+    b = BookInStock.new '', 10.1
+    p b.type_of_price
+  end
+
+  def test_4
+    puts (100).class
+    puts (100.0).class
+    puts (100*100.0).class
+    puts (100.0*100).class
+    puts (100/100.0).class
+    puts (100.0/100).class
+  end
+
+  def test_5
+    b = BookInStock.new '', 100
+    puts b.price
+    puts b.price_in_cents
+
+    b.price = 500
+    puts b.price
+    puts b.price_in_cents
+
+    b.price_in_cents = 700
+    puts b.price
+    puts b.price_in_cents
+  end
+
+end
