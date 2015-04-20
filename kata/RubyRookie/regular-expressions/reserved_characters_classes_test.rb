6c53ae4960e1bda7e7b77c22516816217d917cde
teamnautilus <nautilus@shoppydoo.com>
2015-04-20 16:21:15 +0200

    regexp.

--- kata/RubyRookie/regular-expressions/reserved_characters_classes_test.rb
+++ kata/RubyRookie/regular-expressions/reserved_characters_classes_test.rb
@@ -34,4 +34,54 @@ class ReservedCharactersClassesTest < Test::Unit::TestCase
     @show.regex(value, /[a-z][^a-z]/) # un carattere seguito da un non carattert: [e ]
   end
 
+  def test_4
+    value = 'pRice 12 dollarz'
+    @show.regex(value, /[0-9]/) # 1
+    @show.regex(value, /\d/) # 1
+  end
+
+  def test_5
+    @show.regex('Über', /(?a)\w+/)
+    @show.regex('Über', /(?d)\w+/)
+    @show.regex('Über', /(?u)\w+/)
+
+    @show.regex('Über', /(?a)\W+/)
+    @show.regex('Über', /(?d)\W+/)
+    @show.regex('Über', /(?u)\W+/)
+  end
+
+  def test_6
+    @show.regex('Price $12.', /[aeiou]/)
+    @show.regex('Price $12.', /[[:digit:]]/)
+    @show.regex('Price $12.', /[[:space:]]/)
+  end
+
+  def test_7
+    value = 'see [The PickAxe-page 123]'
+
+    @show.regex(value, /[\[]/)
+    @show.regex(value, /[\-]/)
+
+    @show.regex(value, /[\d\-]/) # il primo dei due che incontra è il match del trattino.
+  end
+
+  def test_8
+    p 'now is the time'.gsub(/[[a-z]&&[^aeiou]]/, '*') # Sostituisco tutte le non vocali.
+  end
+
+  def test_9
+    value = 'δy / δx = 2πr' # occhio che in realtà il simbolo di derivata parziale è diverso. Qui c'è un delta...
+    @show.regex(value, /\p{Alnum}/)
+    @show.regex(value, /\p{Digit}/)
+    @show.regex(value, /\p{Greek}/)
+  end
+
+  def test_10
+    value = 'It costs $12.'
+
+    @show.regex(value, /c.s/)
+    @show.regex(value, /./)
+    @show.regex(value, /\./)
+  end
+
 end
