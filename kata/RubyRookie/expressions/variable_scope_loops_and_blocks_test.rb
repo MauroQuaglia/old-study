e1647699bd2a94caa05a0816de623e522d51e3ad
teamnautilus <nautilus@shoppydoo.com>
2015-05-19 12:28:14 +0200

    Inizio capitolo eccezioni.

--- kata/RubyRookie/expressions/variable_scope_loops_and_blocks_test.rb
+++ kata/RubyRookie/expressions/variable_scope_loops_and_blocks_test.rb
@@ -2,37 +2,29 @@ require 'test/unit'
 
 class VariableScopeLoopsAndBlocksTest < Test::Unit::TestCase
 
-  def test_9
+  def test_1
     a = 0
     while a < 3
       puts a
-      i = 9
+      i = 9 # la definisco qua, ma la vedo anche fuori.
       a += 1
     end
     puts i
   end
 
-  def test_10
+  def test_2
     [1,2,3].each do |x|
       y = x + 1
     end
-    puts [x,y]
+    puts y # non la vede.
   end
 
-  def test_11
-    x = 'ciao'
-    y = 'miao'
+  def test_3
+    y = 'X'
     [1,2,3].each do |x|
       y = x + 1
     end
-    puts [x,y]
+    puts y # la vede perché era precedentemente definita, ed è stata influenzata dal blocco.
   end
 
-  def test_12
-    x =
-    [1,2,3].each do |x|
-      x + 1
-    end
-    p x
-  end
 end
