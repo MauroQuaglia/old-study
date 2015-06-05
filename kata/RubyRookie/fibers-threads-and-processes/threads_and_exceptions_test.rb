c51bfaf3747002f7642c9ebba8b8c93e93f7128c
teamnautilus <nautilus@shoppydoo.com>
2015-06-05 12:52:13 +0200

    thread.

--- kata/RubyRookie/fibers-threads-and-processes/threads_and_exceptions_test.rb
+++ kata/RubyRookie/fibers-threads-and-processes/threads_and_exceptions_test.rb
@@ -13,6 +13,29 @@ class ThreadsAndExceptionsTest < Test::Unit::TestCase
     sleep 1 # Aspetto perchè non essendoci il join può darsi che il test finisca prima che il thread abbia scritto il suo numero.
   end
 
+  def test_2
+    # qui uccide anche il main thread.
+    Thread.abort_on_exception = true
+    4.times do |number|
+      Thread.new(number) do |i|
+        fail if i == 1
+        print "#{i}\n"
+      end
+    end
+    sleep(2)
+  end
+
+  def test_3
+    # qui uccide solo il thread che ha dato eccezione
+    Thread.abort_on_exception = false
+    4.times do |number|
+      Thread.new(number) do |i|
+        fail if i == 1
+        print "#{i}\n"
+      end
+    end
+    sleep(2)
+  end
 
 end
 
