1ce439178da1de7b15cb576c07e3fb04652d461b
teamnautilus <nautilus@shoppydoo.com>
2015-06-09 12:52:57 +0200

    Processi parallelli.

---
+++ kata/RubyRookie/fibers-threads-and-processes/spawning_new_processes_test.rb
@@ -0,0 +1,17 @@
+require 'test/unit'
+
+class SpawningNewProcessesTest < Test::Unit::TestCase
+
+  def test_1
+    `date` # non scrive su console del programma
+    system('date') # scrive
+  end
+
+  def test_2
+    puts `date` # scrive
+    puts system('date') # scrive e torna true
+  end
+
+
+end
+
