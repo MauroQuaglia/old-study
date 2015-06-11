18676b9f95df7e957aa49f1d7aae799bca644314
lnx-7pixel4 <nautilus@shoppydoo.com>
2015-06-11 12:51:46 +0200

    unit test.

--- kata/RubyRookie/fibers-threads-and-processes/blocks_and_subprocesses_test.rb
+++ kata/RubyRookie/fibers-threads-and-processes/blocks_and_subprocesses_test.rb
@@ -3,7 +3,33 @@ require 'test/unit'
 class BlocksAndSubprocessesTest < Test::Unit::TestCase
 
   def test_1
+    # f rappresenta il fork
     IO.popen('date') {|f| puts "Date is #{f.gets}"}
+  end
+
+  def test_2
+    #3124
+    fork do
+      puts '1'
+      sleep 1
+      puts '2'
+    end
+    puts '3'
+    sleep 2
+    puts '4'
+  end
+
+  def test_3
+    #1234
+    fork do
+      puts '1'
+      sleep 1
+      puts '2'
+    end
+    Process.wait
+    puts '3'
+    sleep 2
+    puts '4'
   end
 
 end
