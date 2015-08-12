e3434fcddd95ee0fa58ceddb2baf28069c142ca4
lnx-7pixel4 <nautilus@shoppydoo.com>
2015-08-12 14:59:23 +0200

    Commnad line.

---
+++ projects/RSpecRookie/core/command-line/tagged_spec.rb
@@ -0,0 +1,9 @@
+RSpec.describe "group with tagged specs" do
+  it "example I'm working now", :focus => true do; end
+  it "example I'm working now, too", :mq do; end
+  it "special example with string", :type => 'special' do; end
+  it "special example with symbol", :type => :special do; end
+  it "slow example", :skip => true do; end
+  it "ordinary example", :speed => 'slow' do; end
+  it "untagged example" do; end
+end
