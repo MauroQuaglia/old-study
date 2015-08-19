2b4b1e6fdf7fe6ed11d38562d06d1f54a5c571cd
lnx-7pixel4 <nautilus@shoppydoo.com>
2015-08-19 17:06:50 +0200

    helper

---
+++ projects/RSpecRookie/core/helper-methods/include_module_spec.rb
@@ -0,0 +1,13 @@
+require_relative 'helpers'
+
+RSpec.configure do |c|
+  c.include Helpers
+end
+
+RSpec.describe 'an example group' do
+
+  it 'has access to the helper methods defined in the module' do
+    expect(help).to be(:available)
+  end
+
+end
