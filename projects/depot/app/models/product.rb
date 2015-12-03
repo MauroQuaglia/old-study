b4ba1e761c5ce631331faeaff9fef619e01e3beb
lnx-7pixel1 <nautilus@shoppydoo.com>
2015-12-03 12:43:53 +0100

    Fissati tutti i test sul Product.

--- projects/depot/app/models/product.rb
+++ projects/depot/app/models/product.rb
@@ -1,7 +1,7 @@
 class Product < ActiveRecord::Base
   validates(:title, :description, :image_url, presence: true) # i campi sono presenti e non vuoti
   validates(:price, numericality: {greater_than_or_equal_to: 0.01}) # il prezzo >= 0.01
-  validates(:title, uniqueness: true) # il titolo non deve essere già presente su db.
+  validates(:title, uniqueness: true, length: {minimum: 10}) # il titolo non deve essere già presente su db.
   validates(:image_url, allow_blank: true,
             format: {
                 with: %r{\.(gif|jpg|png)\Z}i,
--- projects/depot/spec/models/product_spec.rb
+++ projects/depot/spec/models/product_spec.rb
@@ -14,39 +14,43 @@ RSpec.describe Product do
   end
 
   it 'price must be positive' do
-    product = product_for('My title', 'My description', 'my-image-url.png', -1)
+    product = product_for('My good title', 'My description', 'my-image-url.png', -1)
     expect(product.errors[:price].first).to eq('must be greater than or equal to 0.01')
 
-    product = product_for('My title', 'My description', 'my-image-url.png', 0)
+    product = product_for('My good title', 'My description', 'my-image-url.png', 0)
     expect(product.errors[:price].first).to eq('must be greater than or equal to 0.01')
 
-    product = product_for('My title', 'My description', 'my-image-url.png', 1)
+    product = product_for('My good title', 'My description', 'my-image-url.png', 1)
     expect(product.valid?).to be_truthy
   end
 
   it 'image url' do
-    product = product_for('My title', 'My description', 'my-image-url.gif', 1)
+    product = product_for('My good title', 'My description', 'my-image-url.gif', 1)
     expect(product.valid?).to be_truthy
 
-    product = product_for('My title', 'My description', 'my-image-url.doc', 1)
+    product = product_for('My good title', 'My description', 'my-image-url.doc', 1)
     expect(product.invalid?).to be_truthy
   end
 
   it 'fixture is loaded' do
-    expect(Product.all.first[:title]).to eq('Ruby')
+    expect(Product.all.first[:title]).to eq('Ruby Programming')
   end
 
   it 'product is not valid without a unique title' do
+    title_already_existing = products(:ruby).title
+    product = product_for(title_already_existing, 'description', 'image.png', 1000)
 
-
-
-    #product = product_for(products(:ruby).title, 'xxx', 'y.png', 1000)
-
-    #expect(product.invalid?).to be_truthy
-    #expect(product.errors[:title].first).to eq('has already been taken')
+    expect(product.invalid?).to be_truthy
+    expect(product.errors[:title].first).to eq('has already been taken')
+    expect(product.errors[:title].first).to eq(I18n.translate('errors.messages.taken'))
   end
 
+  it 'title must be at least ten characters' do
+    product = product_for('Bit title', 'My description', 'my-image-url.gif', 1)
 
+    expect(product.invalid?).to be_truthy
+    expect(product.errors[:title].first).to eq('is too short (minimum is 10 characters)')
+  end
 
   private
 
