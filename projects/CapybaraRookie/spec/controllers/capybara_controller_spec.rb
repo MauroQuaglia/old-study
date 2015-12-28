deeb35d8d20d161d4dc79b4e79cc35e5fbc8ff55
lnx-7pixel2 <nautilus@shoppydoo.com>
2015-12-28 13:01:42 +0100

    Fix spec helper.

--- projects/CapybaraRookie/spec/controllers/capybara_controller_spec.rb
+++ projects/CapybaraRookie/spec/controllers/capybara_controller_spec.rb
@@ -2,9 +2,19 @@ require 'rails_helper'
 
 RSpec.describe CapybaraController do
 
-  it 'returns http success' do
+  it 'returns http success light' do
     get :index
-    expect(response).to have_http_status(:success)
+    puts response.status
+  end
+
+  it 'returns http success spec decorated', :puts_in_spec_helper => :on do
+    get :index
+    puts response.status
+  end
+
+  it 'returns http success rails decorated', :puts_in_rails_helper => :on do
+    get :index
+    puts response.status
   end
 
 end
--- projects/CapybaraRookie/spec/lib/hello_capybara_spec.rb
+++ projects/CapybaraRookie/spec/lib/hello_capybara_spec.rb
@@ -6,10 +6,12 @@ RSpec.describe HelloCapybara do
     expect(described_class.new.say).to eq('Hello Capybara!')
   end
 
+  # Funziona anche se non carico lo spec_helper.
   it 'should enabled decoration text by spec helper', :puts_in_spec_helper => :on do
     puts described_class.new.say
   end
 
+  # Funziona se carico il rails_helper: require 'rails_helper'
   it 'should enabled decoration text by rails helper', :puts_in_rails_helper => :on do
     puts described_class.new.say
   end
