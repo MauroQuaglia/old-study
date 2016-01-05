20bf86f498dd95ce2d25c4f78a41200a01ab2451
lnx-7pixel2 <nautilus@shoppydoo.com>
2016-01-05 15:08:39 +0100

    headers.

---
+++ projects/CapybaraRookie/spec/controllers/capybara_controller_manage_request_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+RSpec.describe CapybaraController do
+
+  it 'returns http success light' do
+    request.env['HTTP_USER_AGENT'] = 'Googlebot'
+
+    get :index
+
+    expect(request.env['HTTP_USER_AGENT']).to eq('Googlebot')
+    expect(request.user_agent).to eq('Googlebot')
+  end
+
+end
+
+
