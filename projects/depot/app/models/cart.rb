6cf14f912dce580c12ce126c9339732a6eaf41d5
lnx-7pixel1 <nautilus@shoppydoo.com>
2016-02-01 15:03:34 +0100

    Quantità.

--- projects/depot/app/models/cart.rb
+++ projects/depot/app/models/cart.rb
@@ -1,3 +1,18 @@
 class Cart < ActiveRecord::Base
   has_many :line_items, dependent: :destroy
+
+  def add_product(product_id)
+    item = line_items.find_by(product_id: product_id)
+
+    item.nil? ? line_items.create(product) : item.update(:quantity)
+  end
+
+  # #<LineItem
+  # id: 3,
+  #     product_id: 68, --> questo è quello che devo cercare.
+  #     cart_id: 1,  questo ce l'ho da fuori, è lui
+  #     created_at: "2016-01-27 13:57:37",
+  #     updated_at: "2016-01-27 13:57:37",
+  #     quantity: 1
+  # >
 end
