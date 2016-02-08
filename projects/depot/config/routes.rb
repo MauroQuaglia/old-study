8d3ff1423588c52d1c467cf3e9595204e3f40e8f
lnx-7pixel1 <nautilus@shoppydoo.com>
2016-02-08 15:03:22 +0100

    Carrello valido.

--- projects/depot/config/routes.rb
+++ projects/depot/config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
 
+  get 'store/index'
+
   resources :line_items
 =begin
 
@@ -17,18 +19,14 @@ Rails.application.routes.draw do
 
 =end
 
-
-
   resources :carts
-  get 'store/index'
-
   resources :products
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'store#index'
+  root 'store#index', as: 'store'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
