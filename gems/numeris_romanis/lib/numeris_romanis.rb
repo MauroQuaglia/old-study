17ccc236e157565966872d8bceb4694d10d7f1af
lnx-7pixel1 <nautilus@shoppydoo.com>
2016-02-12 14:40:23 +0100

    Refactor.

--- gems/numeris_romanis/lib/numeris_romanis.rb
+++ gems/numeris_romanis/lib/numeris_romanis.rb
@@ -1,36 +1,34 @@
 class NumerisRomanis
 
+  MAP = {1000 => 'M', 900 => 'CM', 500 => 'D', 400 => 'CD', 100 => 'C', 90 => 'XC',
+         50 => 'L', 40 => 'XL', 10 => 'X', 9 => 'IX', 5 => 'V', 4 => 'IV', 1 => 'I'
+  }
+
   def initialize
-    @map = {
-        1000 => 'M',
-        900 => 'CM',
-        500 => 'D',
-        400 => 'CD',
-        100 => 'C',
-        90 => 'XC',
-        50 => 'L',
-        40 => 'XL',
-        10 => 'X',
-        9 => 'IX',
-        5 => 'V',
-        4 => 'IV',
-        1 => 'I'
-    }
+    @roman = {}
+    (1..3999).each do |number|
+      @roman[number] = roman_for(number)
+    end
   end
 
   def to_roman(value)
+    @roman[value]
+  end
+
+  def to_decimal(roman)
+    @roman.key(roman)
+  end
+
+  private
+
+  def roman_for(value)
     conversion = ''
-    @map.each do |decimal, roman|
+    MAP.each do |decimal, roman|
       result = value.divmod(decimal)
       conversion << roman * result.first
       value = result.last
     end
     conversion
   end
-
-  def to_decimal(roman)
-
-  end
-
 
 end
--- gems/numeris_romanis/spec/lib/numeris_romanis_spec.rb
+++ gems/numeris_romanis/spec/lib/numeris_romanis_spec.rb
@@ -2,7 +2,11 @@ require 'numeris_romanis'
 
 RSpec.describe NumerisRomanis do
 
-  it 'should convert decimal numbers correctly' do
+  it 'should convert roman numbers correctly' do
+    numbers = described_class.new
+
+    expect(numbers.to_decimal('I')).to eq(1)
+    expect(numbers.to_decimal('MMMCMXCIX')).to eq(3999)
   end
 
 end
