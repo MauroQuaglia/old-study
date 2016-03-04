f078984ca3d4fb5790a809e25f92379bde620bdc
lnx-7pixel1 <nautilus@shoppydoo.com>
2016-03-04 14:53:47 +0100

    Oreder.

--- projects/depot/app/models/line_item.rb
+++ projects/depot/app/models/line_item.rb
@@ -1,6 +1,7 @@
 class LineItem < ActiveRecord::Base
+  belongs_to(:order)
   belongs_to :product # Da LineItem posso risalire al Product.
-  belongs_to :cart    # Da LineItem posso risalire al Cart.
+  belongs_to :cart # Da LineItem posso risalire al Cart.
 
   def total_price
     product.price * quantity
