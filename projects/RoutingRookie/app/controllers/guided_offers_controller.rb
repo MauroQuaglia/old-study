1c3f2682c35feac0586b0729bad1fa9ff78b5c93
nautilus@albacore <nautilus@shoppydoo.com>
2016-03-25 14:41:40 +0100

    Fix test banali.

--- projects/RoutingRookie/app/controllers/guided_offers_controller.rb
+++ projects/RoutingRookie/app/controllers/guided_offers_controller.rb
@@ -1,6 +1,8 @@
 class GuidedOffersController < ApplicationController
 
   def listing
+    puts "Category: [#{params[:category]}]; Page: [#{params[:page]}]"
+    return bad_request if (!params[:category].present? || !params[:page].present?)
     render text: "Category: [#{params[:category]}]; Page: [#{params[:page]}]"
   end
 
--- projects/RoutingRookie/spec/controllers/guided_offers_controller_spec.rb
+++ projects/RoutingRookie/spec/controllers/guided_offers_controller_spec.rb
@@ -2,9 +2,38 @@ require 'rails_helper'
 
 RSpec.describe GuidedOffersController do
 
-  it 'rejects empty parameters' do
+  it 'rejects nil parameters' do
     get :listing
     expect(response).to have_http_status(400)
+    expect(controller.params[:category]).to be_nil
+    expect(controller.params[:page]).to be_nil
+
+    get :listing, {category: 'cellulari'}
+    expect(response).to have_http_status(400)
+    expect(controller.params[:category]).to eq('cellulari')
+    expect(controller.params[:page]).to be_nil
+
+    get :listing, {page: '1'}
+    expect(response).to have_http_status(400)
+    expect(controller.params[:category]).to be_nil
+    expect(controller.params[:page]).to eq('1')
+  end
+
+  it 'rejects empty parameters' do
+    get :listing, {category: '', page: ''}
+    expect(response).to have_http_status(400)
+    expect(controller.params[:category]).to be_empty
+    expect(controller.params[:page]).to be_empty
+
+    get :listing, {category: 'cellulari', page: ''}
+    expect(response).to have_http_status(400)
+    expect(controller.params[:category]).to eq('cellulari')
+    expect(controller.params[:page]).to be_empty
+
+    get :listing, {category: '', page: '1'}
+    expect(response).to have_http_status(400)
+    expect(controller.params[:category]).to be_empty
+    expect(controller.params[:page]).to eq('1')
   end
 
 end
