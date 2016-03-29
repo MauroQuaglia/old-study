477d8c08d8cd2088306459e91dbe8ea32e08622d
nautilus@albacore <nautilus@shoppydoo.com>
2016-03-29 12:13:35 +0200

    fix.

--- projects/RoutingRookie/app/controllers/guided_offers_controller.rb
+++ projects/RoutingRookie/app/controllers/guided_offers_controller.rb
@@ -6,13 +6,32 @@ class GuidedOffersController < ApplicationController
     category = params[:category]
     page = params[:page]
 
+    # Validazione della presenza dei parametri.
     if not (category.present? && page.present?)
       return bad_request
     end
 
+    # A questo punto entrambe i parametri sono presenti...
+
+    # Validazione del parametro page.
+    if not is_integer?(page)
+      return bad_request
+    end
+
+    # Dato che c'è il parametri page (indipendentemente da cosa esso sia)
+    #if (category == 'all' || category == 'macro')
+   #   return bad_request
+    #end
 
 
     render text: "Category: [#{params[:category]}]; Page: [#{params[:page]}]"
   end
+
+  private
+
+  def is_integer?(string)
+    true if Integer(string) rescue false
+  end
+
 
 end
--- projects/RoutingRookie/spec/controllers/guided_offers_controller_spec.rb
+++ projects/RoutingRookie/spec/controllers/guided_offers_controller_spec.rb
@@ -36,4 +36,49 @@ RSpec.describe GuidedOffersController do
     expect(controller.params[:page]).to eq('1')
   end
 
+  it 'manage page parameter' do
+    get :listing, {category: 'any', page: 'a'}
+    expect(response).to have_http_status(400)
+    expect(controller.params[:category]).to eq('any')
+    expect(controller.params[:page]).to eq('a')
+
+    get :listing, {category: 'any', page: '-1'}
+    expect(response).to have_http_status(400)
+    expect(controller.params[:category]).to eq('any')
+    expect(controller.params[:page]).to eq('-1')
+
+    get :listing, {category: 'any', page: '0'}
+    expect(response).to have_http_status(400)
+    expect(controller.params[:category]).to eq('any')
+    expect(controller.params[:page]).to eq('0')
+
+    get :listing, {category: 'any', page: '1'}
+    expect(response).to have_http_status(200)
+    expect(controller.params[:category]).to eq('any')
+    expect(controller.params[:page]).to eq('1')
+
+    get :listing, {category: 'any', page: '1000000'}
+    expect(response).to have_http_status(410)
+    expect(controller.params[:category]).to eq('any')
+    expect(controller.params[:page]).to eq('1000000')
+  end
+
+
+  xit 'manage category with valid page parameters' do
+    get :listing, {category: 'all', page: '1'}
+    expect(response).to have_http_status(400)
+    expect(controller.params[:category]).to eq('all')
+    expect(controller.params[:page]).to eq('1')
+
+    get :listing, {category: 'telefonia', page: '1'}
+    expect(response).to have_http_status(400)
+    expect(controller.params[:category]).to eq('telefonia')
+    expect(controller.params[:page]).to eq('1')
+
+    get :listing, {category: 'cellulari', page: '1'}
+    expect(response).to have_http_status(200)
+    expect(controller.params[:category]).to eq('cellulari')
+    expect(controller.params[:page]).to eq('1')
+  end
+
 end
