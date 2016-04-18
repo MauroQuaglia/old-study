1bab6ff4f21cdfd4ad407638f75c081501536c57
nautilus@octopus <nautilus@shoppydoo.com>
2016-04-18 14:02:04 +0200

    fix.

--- projects/RoutingRookie/config/routes.rb
+++ projects/RoutingRookie/config/routes.rb
@@ -1,24 +1,19 @@
 Rails.application.routes.draw do
 
   get '/prezzi_(:category)_(:page).aspx', to: 'guided_offers#listing', as: :guided_offers_listing
-  get '/prezzi_(:category).aspx'        , to: 'guided_offers#table'  , as: :guided_offers_table
+  get '/prezzi_(:category).aspx', to: 'guided_offers#table', as: :guided_offers_table
 
+  get '/prezzo_(:category)_(:search)^min(:min_price)^max(:max_price).aspx',                        to: 'listings#price'
+  get '/prezzo_(:category)_(:search)^min(:min_price).aspx', default: {max_price: Float::INFINITY}, to: 'listings#price'
+  get '/prezzo_(:category)_(:search)^max(:max_price).aspx', default: {min_price: 0},               to: 'listings#price'
+  get '/prezzo_(:category)_(:search).aspx', default: {min_price: 0, max_price: Float::INFINITY},   to: 'listings#price'
 
-  # get '/prezzo_(:category)_(:search)^min(:min_price)^max(:max_price).aspx', to: 'listings#min_max_price'
-  # get '/prezzo_(:category)_(:search)^min(:min_price).aspx', to: 'listings#min_price'
-  # get '/prezzo_(:category)_(:search)^max(:max_price).aspx', to: 'listings#max_price'
-  #
-  # get '/prezzo_(:category)_(:search).aspx', to: 'listings#price' # Ricordarsi di mettere i default per i prezzi.
-
-  # '/prezzo__%5Emin%5Emax.aspx'
-  get '/prezzo_(:category)_(:search)(^min(:min_price))(^max(:max_price)).aspx', to: 'listings#price', constraints: { search: /[^\^%5E]+/x }
+  #get '/prezzo_(:category)_(:search)(^min(:min_price))(^max(:max_price)).aspx', to: 'listings#price', constraints: { search: /[^\^%5E]+/x }
 
 
   match '*url', to: 'application#not_acceptable', via: :all
 
 end
-
-
 
 
 # test routing con tutte le casistiche di mpping parametri
--- projects/RoutingRookie/spec/routing/guided_offers_routing_spec.rb
+++ projects/RoutingRookie/spec/routing/guided_offers_routing_spec.rb
@@ -4,7 +4,6 @@ RSpec.describe 'Routes' do
 
   context '/prezzo_CATEGORY_SEARCH.aspx' do
 
-    # Ricordarsi di mettere i default per i prezzi.
     it 'understand parameters' do
       expect(get: '/prezzo__.aspx').to route_to({controller: 'listings', action: 'price'})
       expect(get: '/prezzo__nokia.aspx').to route_to({controller: 'listings', action: 'price', search: 'nokia'})
@@ -28,20 +27,6 @@ RSpec.describe 'Routes' do
     end
 
 
-  end
-
-  context '/prezzo_CATEGORY_SEARCH^min(:MIN_PRICE).aspx' do
-
-    it 'understand parameters' do
-      expect(get: '/prezzo__%5Emin.aspx').to route_to({controller: 'listings', action: 'min_price'})
-      expect(get: '/prezzo__%5Emin100.aspx').to route_to({controller: 'listings', action: 'min_price', min_price: '100'})
-      expect(get: '/prezzo__nokia%5Emin.aspx').to route_to({controller: 'listings', action: 'min_price', search: 'nokia'})
-      expect(get: '/prezzo__nokia%5Emin100.aspx').to route_to({controller: 'listings', action: 'min_price', search: 'nokia', min_price: '100'})
-      expect(get: '/prezzo_cellulari_%5Emin.aspx').to route_to({controller: 'listings', action: 'min_price', category: 'cellulari'})
-      expect(get: '/prezzo_cellulari_%5Emin100.aspx').to route_to({controller: 'listings', action: 'min_price', category: 'cellulari', min_price: '100'})
-      expect(get: '/prezzo_cellulari_nokia%5Emin.aspx').to route_to({controller: 'listings', action: 'min_price', category: 'cellulari', search: 'nokia'})
-      expect(get: '/prezzo_cellulari_nokia%5Emin100.aspx').to route_to({controller: 'listings', action: 'min_price', category: 'cellulari', search: 'nokia', min_price: '100'})
-    end
   end
 
   context '/prezzo_CATEGORY_SEARCH^max(:MAX_PRICE).aspx' do
