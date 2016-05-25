eda9983e886533276320e0a4e3b16dac8a2b8f70
nautilus@octopus <nautilus@shoppydoo.com>
2016-05-25 14:47:38 +0200

    Active Record.

--- projects/depot/app/models/order.rb
+++ projects/depot/app/models/order.rb
@@ -14,6 +14,8 @@ class Order < ActiveRecord::Base
   has_many(:line_items, dependent: :destroy)
   validates(:name, :address, :email, presence: true)
   validates(:pay_type, inclusion: PAYMENT_TYPES)
+  scope :last_n_days, lambda { |days| where('updated < ?', days) } # with parameter
+  scope :last_7_days, -> { where('updated < 7') }                  # without parameter
 
   def add_line_items_from_cart(cart)
     cart.line_items.each do |item|
@@ -22,4 +24,7 @@ class Order < ActiveRecord::Base
     end
   end
 
+  def self.last_week
+    last_n_days(7)
+  end
 end
